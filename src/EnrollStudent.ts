f636974b4174ff9c12e86e6550c65b8383ba2339
Thiago Turim Carvalho <thiago.carvalho@dextra-sw.com>
2021-05-18 09:25:05 -0300

    feat: not enroll without valid student cpf

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -1,15 +1,21 @@
+import { CpfValidator } from "./CpfValidator";
 import { Student } from "./Student";
 
 export class EnrollStudent {
   private students: Student[] = [];
 
+  constructor(private readonly cpfValidator: CpfValidator) {}
+
   async execute(enrollmentRequest: EnrollmentRequest): Promise<void> {
-    const { name } = enrollmentRequest.student;
+    const { name, cpf } = enrollmentRequest.student;
     if (!this.isValidName(name)) {
       throw new Error("Invalid student name.");
     }
     if (this.students.some((register) => register.name === name)) {
       throw new Error("Enrollment with duplicated student is not allowed.");
+    }
+    if (!this.cpfValidator.isValid(cpf)) {
+      throw new Error("Invalid student cpf.");
     }
     this.students.push(enrollmentRequest.student);
   }
