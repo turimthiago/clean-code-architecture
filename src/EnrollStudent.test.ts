fcf3b695ab6612eb13b92270ff95cdfab8e6e9bc
Thiago Turim Carvalho <thiago.carvalho@dextra-sw.com>
2021-05-18 09:25:19 -0300

    test: ensure not enroll without valid student cpf

--- src/EnrollStudent.test.ts
+++ src/EnrollStudent.test.ts
@@ -1,17 +1,25 @@
+import { CpfValidator } from "./CpfValidator";
 import { EnrollStudent } from "./EnrollStudent";
 import { Student } from "./Student";
 
 describe("EnrollStudent usecase", () => {
   test("Should not enroll without valid student name", async () => {
-    const sut = new EnrollStudent();
-    const student = { name: "Ana" };
+    const sut = new EnrollStudent(new CpfValidator());
+    const student = { name: "Ana", cpf: "00902486004" };
     const promise = sut.execute({ student });
     await expect(promise).rejects.toThrow(new Error("Invalid student name."));
   });
 
+  test("Should not enroll without valid student cpf", async () => {
+    const sut = new EnrollStudent(new CpfValidator());
+    const student = { name: "Ana Silva", cpf: "123.456.789-99" };
+    const promise = sut.execute({ student });
+    await expect(promise).rejects.toThrow(new Error("Invalid student cpf."));
+  });
+
   test("Should not enroll duplicated student", async () => {
-    const sut = new EnrollStudent();
-    const student = { name: "John Winston Lennon" };
+    const sut = new EnrollStudent(new CpfValidator());
+    const student = { name: "John Winston Lennon", cpf: "93093168023" };
     await sut.execute({ student });
     const promise = sut.execute({ student });
     await expect(promise).rejects.toThrow(
