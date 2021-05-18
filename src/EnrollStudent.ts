11519cea091d99811dba62dd92f21090555b5d61
Thiago Turim Carvalho <thiago.carvalho@dextra-sw.com>
2021-05-18 09:08:39 -0300

    feat: not enroll duplicated student

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -1,11 +1,17 @@
 import { Student } from "./Student";
 
 export class EnrollStudent {
+  private students: Student[] = [];
+
   async execute(enrollmentRequest: EnrollmentRequest): Promise<void> {
     const { name } = enrollmentRequest.student;
     if (!this.isValidName(name)) {
-      throw new Error("Invalid student name");
+      throw new Error("Invalid student name.");
     }
+    if (this.students.some((register) => register.name === name)) {
+      throw new Error("Enrollment with duplicated student is not allowed.");
+    }
+    this.students.push(enrollmentRequest.student);
   }
 
   private isValidName(name: string): boolean {
