53efd09217a7c4a9763cb00cafff6482fb872ef6
Thiago Turim Carvalho <turimthiago@gmail.com>
2021-05-29 23:35:03 -0300

    feat: generate registration code

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -13,6 +13,12 @@ export class EnrollStudent {
       enrollmentRequest.student.name,
       enrollmentRequest.student.cpf
     );
+    //const registrationCode = new Registration()
+    const code = `${new Date().getFullYear()}${enrollmentRequest.level}${
+      enrollmentRequest.module
+    }${enrollmentRequest.class}${(this.enrollments.length + 1)
+      .toString()
+      .padStart(4, "0")}`;
     const existingEnrollment = this.enrollments.find(
       (enrollment) =>
         enrollment.student.cpf.value === enrollmentRequest.student.cpf
@@ -21,6 +27,12 @@ export class EnrollStudent {
       throw new Error("Enrollment with duplicated student is not allowed");
     const enrollment = {
       student,
+      level: enrollmentRequest.level,
+      module: enrollmentRequest.module,
+      class: enrollmentRequest.class,
+      registration: {
+        code
+      }
     };
     this.enrollments.push(enrollment);
     return enrollment;
@@ -32,4 +44,7 @@ export type EnrollmentRequest = {
     name: string;
     cpf: string;
   };
+  level: string;
+  module: string;
+  class: string;
 };
