c97f123593acc6ed5af151d119fc454872c76d2a
Thiago Turim Carvalho <turimthiago@gmail.com>
2021-05-29 23:35:17 -0300

    test: ensure generate registration code

--- src/EnrollStudent.test.ts
+++ src/EnrollStudent.test.ts
@@ -5,7 +5,12 @@ import { Student } from "./Student";
 describe("EnrollStudent usecase", () => {
   test("Should not enroll without valid student name", async () => {
     const sut = new EnrollStudent();
-    const enrollRequest = { student: { name: "Ana", cpf: "00902486004" } };
+    const enrollRequest = {
+      student: { name: "Ana", cpf: "00902486004" },
+      level: "EM",
+      module: "1",
+      class: "A",
+    };
     const promise = sut.execute(enrollRequest);
     await expect(promise).rejects.toThrow(new Error("Invalid name"));
   });
@@ -14,6 +19,9 @@ describe("EnrollStudent usecase", () => {
     const sut = new EnrollStudent();
     const enrollRequest = {
       student: { name: "Ana Silva", cpf: "123.456.789-99" },
+      level: "EM",
+      module: "1",
+      class: "A",
     };
     const promise = sut.execute(enrollRequest);
     await expect(promise).rejects.toThrow(new Error("Invalid cpf"));
@@ -23,11 +31,26 @@ describe("EnrollStudent usecase", () => {
     const sut = new EnrollStudent();
     const enrollRequest = {
       student: { name: "John Winston Lennon", cpf: "93093168023" },
+      level: "EM",
+      module: "1",
+      class: "A",
     };
     await sut.execute(enrollRequest);
     const promise = sut.execute(enrollRequest);
     await expect(promise).rejects.toThrow(
       new Error("Enrollment with duplicated student is not allowed")
     );
+  });
+
+  test("Should generate enrollment code ", async () => {
+    const sut = new EnrollStudent();
+    const enrollRequest = {
+      student: { name: "John Winston Lennon", cpf: "93093168023" },
+      level: "EM",
+      module: "1",
+      class: "A",
+    };
+    const registration = await sut.execute(enrollRequest);
+    expect(registration.registration.code).toEqual("2021EM1A0001")
   });
 });
